26b5ed0c89ff370d52624194ab39fc30ad888477
Gabriel Navas <gabnavas.dev@gmail.com>
2021-09-28 14:08:25 -0300

    add cors on project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import modules.books.models as book_models
 import modules.users.models as users_models
 
@@ -6,6 +8,18 @@ from modules.books.controllers import router as books_router
 from modules.users import token_router, users_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(books_router)
 app.include_router(token_router)
